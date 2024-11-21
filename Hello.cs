734887d7c8d155b2d9c77f8db1a19e23a7226033
Kerven Jumayev <kerwen.jumazew@gmail.com>
2024-11-21 08:22:04 +0300

    add some arrays

--- Hello.cs
+++ Hello.cs
@@ -6,7 +6,21 @@ namespace hello
 	{
 		public static void Main()
 		{
-			Console.WriteLine("Hello World!"); 
+			int[] arrayOne = {1,3,5,7,9}; 
+			int [] arrayTwo, arrayThree; 
+			arrayTwo = arrayOne; 
+			arrayThree = new int[arrayOne.Length]; 
+			for(int i =0; i<arrayOne.Length; i++)
+			{
+				arrayThree[i] = arrayOne[i];
+			}
+
+			for(int i =0; i<arrayThree.Length; i++)
+			{
+				Console.Write(arrayThree[i]);
+			}
+			Console.ReadLine(); 
+			
 		}
 	}
 }
